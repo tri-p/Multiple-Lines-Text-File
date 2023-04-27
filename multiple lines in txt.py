cfdf28c76caf51f3a2d596fc4e3055ad916bd9dc
Katrina Pabuna <kinapabuna@gmail.com>
2023-04-27 12:00:33 +0800

    Ask the user if there are more lines; use a while loop

--- multiple lines in txt.py
+++ multiple lines in txt.py
@@ -4,10 +4,14 @@
 # Open mylife.txt (append)
 with open("mylife.txt", "a") as output_lines:
     # Use a loop to repeat the process until it becomes false
+    while True:
         # Ask the user to enter a line
         line = input("Enter line: ")
         # Write the line onto the text file
         output_lines.write(line + "\n")
         # Ask the user if there are more lines
+        more_line = input("Are there more lines (y/n)? ").lower()
         # If no, end the loop
+        if more_line == "n":
+            break
         # If the user entered an invalid answer, print 'error'
