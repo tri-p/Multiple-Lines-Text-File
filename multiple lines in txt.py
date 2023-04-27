d97b501c4b8cbf75cf130e3084cdb6da4ac8f616
Katrina Pabuna <kinapabuna@gmail.com>
2023-04-27 12:19:07 +0800

    Final Output (with design)

--- multiple lines in txt.py
+++ multiple lines in txt.py
@@ -6,14 +6,18 @@ with open("mylife.txt", "a") as output_lines:
     # Use a loop to repeat the process until it becomes false
     while True:
         # Ask the user to enter a line
-        line = input("Enter line: ")
+        print("\n", "\033[93m=" * 80, "\n")
+        line = input("\033[92mEnter line: \033[97m")
         # Write the line onto the text file
-        output_lines.write(line + "\n")
+        output_lines.write("=" * 80 + "\n" + line + "\n")
         # Ask the user if there are more lines
-        more_line = input("Are there more lines (y/n)? ").lower()
+        print("\n", "\033[93m=" * 80, "\n")
+        more_line = input("\033[96mAre there more lines (y/n)? \033[97m").lower()
         # If no, end the loop
         if more_line == "n":
+            print("\n", "\033[93m=" * 80, "\n")
+            output_lines.write("=" * 80 + "\n")
             break
         # If the user entered an invalid answer, print 'error'
         elif more_line.lower() != 'y':
-            print("Error: Invalid input. Please enter 'y' or 'n'.")
+            print("\033[91mError: Invalid input. Please enter 'y' or 'n'.")
