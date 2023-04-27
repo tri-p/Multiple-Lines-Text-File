4e1f29b6c925646889bb5454ea7bac02c7980f18
Katrina Pabuna <kinapabuna@gmail.com>
2023-04-27 11:46:07 +0800

    Open mylife.txt (append)

---
+++ multiple lines in txt.py
@@ -0,0 +1,5 @@
+# PABUNA, KATRINA B. 
+# Write a method in python to write multiple line of text contents into a text file mylife.txt
+
+# Open mylife.txt (append)
+with open("mylife.txt", "a") as output_lines:
