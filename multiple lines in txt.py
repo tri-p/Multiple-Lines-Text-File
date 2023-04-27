5792529d7b985490bce22e0b105548bde742d186
Katrina Pabuna <kinapabuna@gmail.com>
2023-04-27 11:55:42 +0800

    Ask user to enter a line; write it onto the text file

--- multiple lines in txt.py
+++ multiple lines in txt.py
@@ -3,3 +3,11 @@
 
 # Open mylife.txt (append)
 with open("mylife.txt", "a") as output_lines:
+    # Use a loop to repeat the process until it becomes false
+        # Ask the user to enter a line
+        line = input("Enter line: ")
+        # Write the line onto the text file
+        output_lines.write(line + "\n")
+        # Ask the user if there are more lines
+        # If no, end the loop
+        # If the user entered an invalid answer, print 'error'
